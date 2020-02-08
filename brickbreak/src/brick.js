306e0e5a80233b2e9326059322e9c4323703fefc
Gavin Nishizawa <ghn6069@rit.edu>
2020-02-08 17:57:57 -0500

    Working on brick component state

--- brickbreak/src/brick.js
+++ brickbreak/src/brick.js
@@ -2,10 +2,29 @@
 import React from 'react';
 
 export default class Brick extends React.Component {
-    render() {
-      return (
-      <div>
-      </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: {
+        r: 0,
+        g: 0,
+        b: 0,
+      },
+        u: 0,
+        x: 0,
+        y: 0,
+    };
   }
+
+
+  render() {
+    return (
+      <div style={{
+        position: 'absolute',
+
+        }}
+        />
+    );
+  }
+}
