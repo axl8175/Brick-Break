ccbf494ba8bbd6d1e13373eb06fd5a104fdfebcd
Gavin Nishizawa <ghn6069@rit.edu>
2020-02-09 02:29:11 -0500

    Updates

---
+++ precache-manifest.a1e57d69e0617e8930fcebf8714e6ae4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3855d9fce97aef3948b548cf45f1809b",
+    "url": "/Brick-Break/index.html"
+  },
+  {
+    "revision": "38f08808e73f8ea8c0a6",
+    "url": "/Brick-Break/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "590e76793d9d76d893d3",
+    "url": "/Brick-Break/static/js/2.3e8602fa.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/Brick-Break/static/js/2.3e8602fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f08808e73f8ea8c0a6",
+    "url": "/Brick-Break/static/js/main.a25d4063.chunk.js"
+  },
+  {
+    "revision": "f1725ae526886f9fe7b1",
+    "url": "/Brick-Break/static/js/runtime-main.73397dbb.js"
+  },
+  {
+    "revision": "3d97af79cac9d2ebb5e5fb68bac99630",
+    "url": "/Brick-Break/static/media/Beanz_0.3d97af79.jpg"
+  },
+  {
+    "revision": "4e12eb4b19d95fdbaf7844358bcd0005",
+    "url": "/Brick-Break/static/media/gracies.4e12eb4b.png"
+  }
+]);
