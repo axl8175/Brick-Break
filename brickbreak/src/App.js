9d83bd1c3b6b6d586cb4350f266b7d205cf3d21f
Gavin Nishizawa <ghn6069@rit.edu>
2020-02-09 02:00:45 -0500

    Convert app to class

--- brickbreak/src/App.js
+++ brickbreak/src/App.js
@@ -3,24 +3,29 @@ import './App.css';
 import data from "./things.js";
 import Card from "./card.js";
 
-function renderPlace(place) {
-  return <Card
-  key={place.title}
-  title={place.title}
-  imagelink={place.locallink}
-  text={place.text}/>
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.allcards = data.things.map(this.renderPlace);
+
+  }
+
+  renderPlace(place) {
+    return <Card
+    key={place.title}
+    title={place.title}
+    imagelink={place.locallink}
+    text={place.text}/>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.allcards}
+        </header>
+      </div>
+    );
+  }
 }
-
-const allcards = data.things.map(renderPlace);
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {allcards}
-      </header>
-    </div>
-  );
-}
-
-export default App;
