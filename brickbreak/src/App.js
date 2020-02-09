273696342d7dad189c55ac7891bc19bdacb18558
Gavin Nishizawa <ghn6069@rit.edu>
2020-02-09 02:12:38 -0500

    Add setup for next button

--- brickbreak/src/App.js
+++ brickbreak/src/App.js
@@ -7,8 +7,19 @@ import Card from "./card.js";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.allcards = data.things.map(this.renderPlace);
+    // this.allcards = data.things.map(this.renderPlace);
 
+    this.state = {
+      total: data.things.length,
+      current: 0,
+    };
+
+    this.renderCurrent = this.renderCurrent.bind(this);
+  }
+
+  renderCurrent() {
+    let place = data.things[this.state.current];
+    return this.renderPlace(place);
   }
 
   renderPlace(place) {
@@ -19,11 +30,21 @@ export default class App extends React.Component {
     text={place.text}/>
   }
 
+  toNext() {
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {this.allcards}
+          {this.renderCurrent()}
+          <button type="button"
+            onClick={this.toNext}
+          >
+            Next
+          </button>
+
         </header>
       </div>
     );
