5ad4f0f901d2a73c476bba0d13c2df36fd6c009c
Gavin Nishizawa <ghn6069@rit.edu>
2020-02-08 21:59:10 -0500

    Add constructor to card

--- brickbreak/src/card.js
+++ brickbreak/src/card.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Brickimage from './brickimage';
 
 export default class Card extends React.Component {
-    render() {
-      return <div>
-      <Brickimage link={this.props.imagelink}/>
-      <h1>{this.props.title}</h1>
-      <p>{this.props.text}</p>
-      </div>;
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answered: false,
+    };
   }
+
+  render() {
+    return <div>
+    <Brickimage link={this.props.imagelink}/>
+    <h1>{this.props.title}</h1>
+    <p>{this.props.text}</p>
+    </div>;
+  }
+}
